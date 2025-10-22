07cb9a6542c9410aeb1adad7877f2b99787dec1a
Killian Morere <killian.morere@fms-ea.com>
2025-10-22 16:02:19 +0200

    Changement du prix de int à float et conversion de gramme à kg pour le prix au kilo.

--- product.py
+++ product.py
@@ -6,12 +6,18 @@ class Product:
     products: list = []
 
     @classmethod
-    def get_price(cls, _index:int) -> int:
-        return cls.products[_index][2]
+    def get_price(cls, _index:int, _amount) -> int:
+        price = cls.products[_index][2]
+        price *= _amount
 
-    def __init__(self, _name:str, _stock:int, _price:int, _is_unit:bool):
+        if cls.products[_index][3] is False:
+            price /= 1000
+
+        return price
+
+    def __init__(self, _name:str, _stock:int, _price:float, _is_unit:bool):
         self.name:str = _name
         self.stock:int = _stock
-        self.price:int = _price
+        self.price:float = _price
         self.is_unit:bool = _is_unit
         Product.products.append([_name, _stock, _price, _is_unit])
