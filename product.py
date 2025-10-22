4a67a2ae1971e441afa6500ae6245eda4376cb65
Killian Morere <killian.morere@fms-ea.com>
2025-10-22 15:42:44 +0200

    Première version de get_price qui va récupérer le prix d'un produit et sera utilisé pour faire la somme des produits dans le panier.

--- product.py
+++ product.py
@@ -1,22 +1,17 @@
 from typing import ClassVar
-import product_label
+import label
 
 
 class Product:
-    products: ClassVar[list["Product"]] = []
+    products: list = []
 
     @classmethod
-    def get_price(cls, _product:int) -> int:
-        return cls.products[_product].price
+    def get_price(cls, _index:int) -> int:
+        return cls.products[_index][2]
 
     def __init__(self, _name:str, _stock:int, _price:int, _is_unit:bool):
         self.name:str = _name
         self.stock:int = _stock
         self.price:int = _price
         self.is_unit:bool = _is_unit
-        Product.products.append(self)
-
-
-new_prod = Product("Pomme", 3, 1, True)
-print(new_prod.name)
-print(Product.products[0])
+        Product.products.append([_name, _stock, _price, _is_unit])
