0ac2a6cc42cc19ae1eeb13c1980605f141d84b2b
matthieu-27 <matthieu27@pm.me>
2025-10-23 13:31:52 +0200

    Fix: buy method now uses get_product and remove_product
    - Rename can_buy to is_available
    - Improved print_buy_list display
    - modified buy and is available method arguments to use a string instead of entire product object
    - Updated docstrings

--- client.py
+++ client.py
@@ -17,29 +17,28 @@ class Client:
     total_spent: float
     buy_list: list[tuple[str, float, float]]  # buy history eg: ('Banane', 8, 7.99)
 
-
     def __init__(self, first_name: str, last_name: str):
         self.first_name = first_name.capitalize()
         self.last_name = last_name.capitalize()
         self.total_spent = 0.00
         self.buy_list = []
 
-
     def __repr__(self) -> str:
         return f"{self.last_name} {self.first_name}: {self.total_spent}€"
-
 
     def __str__(self) -> str:
         self.print_buy_list()
         return f"{self.last_name} {self.first_name} vous avez dépensé aujourd'hui: {self.total_spent}€"
 
-
     def print_buy_list(self):
+        """
+        Prints out a formatted receipt of the records in the `buy_list`
+        """
         _date = datetime.today().strftime('%d-%m-%Y')
         _tiny_width, _width = 3,13
         _line = ''.join(['+' + '-' * (_tiny_width + 2)]) + ''.join(['+' + '-' * (_width + 2)])\
                 + ''.join(['+' + '-' * (_tiny_width + 3)]) + '+'
-        print(f"Édition du {_date} - Client {self.first_name} {self.last_name}")
+        print(f"{_date} - {self.first_name} {self.last_name}")
         print(_line)
         print(f"| Qté |    Article    | Prix |")
         print(_line)
@@ -47,40 +46,39 @@ class Client:
         for buy in self.buy_list:
             _name, _quantity, _price = buy
             _total_line_price = round(_price * _quantity, 2)
-            _total_price += _total_line_price
-            _quantity_display = '  ' + str(_quantity).center(_tiny_width) + '  '
-            _price_display = ' ' + ''.join([str(_total_line_price) + '€']).center(_tiny_width+2) + '  '
+            _total_price += round(_total_line_price, 2)
+            _quantity_display = '| ' + str(_quantity).center(_tiny_width) + ' |'
+            _price_display = '|' + ''.join([str(_total_line_price) + '€']).center(_tiny_width+2) + ' |'
             line_display = f"{_quantity_display} {_name.center(_width)} {_price_display}"
             print(line_display)
         print(_line)
-        print(f"| Total                 {_total_price}€  |")
+        print('| ' + 'Total: '.center(_width) + ''.join([_total_price.__str__() + '€']).center(_width) + ' |')
         print(_line)
 
     @staticmethod
-    def can_buy(_product: Product, _quantity:float) -> bool:
+    def is_available(_shop: type[Shop], _name: str, _quantity: float) -> bool:
         """ Checks if `_product` is_unit and returns True if `_product` is available, False otherwise """
+        _product = _shop.get_product(_name)
         return _quantity <= _product.stock if _product.is_unit else _quantity / 1000 <= _product.stock
 
-
-    def buy(self, _shop: Shop, _product: Product, _quantity: float):
+    def buy(self, _shop: type[Shop], _product: Product, _quantity: float):
         """
-        Checks if `can_buy`, if True compute price, remove it from stock and adds it to history,
+        Checks if `is_available`, if True compute price, remove it from stock and adds it to history,
         Prints an error message otherwise.
         """
-        if self.can_buy(_product, _quantity):
-            # self.total_spent += _shop.get_price(_product.price)
+        _product = _shop.get_product(_product.name)
+        if self.is_available(_shop, _product.name, _quantity):
+            _shop.remove_product(_shop.products.index(_product), _quantity)
+            self.total_spent += _shop.get_price(_shop.products.index(_product), _quantity)
             self.buy_list.append((_product.name, _quantity, _product.price))
-            # _product.stock -= int(round(_quantity))
         else:
             print(f"Le produit {_product.name} n'a pas un stock suffisant pour cet achat. (dispo : {_product.stock})\n")
-client = Client("toto", "titi")
 
 
+client = Client("Albert", "Roquefort")
 
-produits = Shop.products
-shop = Shop(produits[0])
+shop = Shop
+clementine = shop.get_product("Clémentine")
+client.buy(shop, clementine, 1)
 
-client.buy(shop, produits[0], 1)
-client.buy(shop, produits[1], 3)
-client.buy(shop, produits[1], 8)
 client.print_buy_list()
